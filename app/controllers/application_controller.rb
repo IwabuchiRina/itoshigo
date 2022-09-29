df3198d36e86f57b0fec288a8f359221111d57d1
Rina Iwabuchi <anps.pyhu922@gmail.com>
2022-09-29 17:18:55 +0900

    ログアウト機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  #deviseコントローラーにストロングパラメータと追加する
+  #before_action :configure_permitted_parameters, if: devise_controller?
+
+  protected
+  def configure_permitted_parameters
+  #サインアップ時にnameのストロングパラメータを追加する
+    devise_parameter_sanitizer.permit(:sing_up, key: [:name])
+  end
 end
