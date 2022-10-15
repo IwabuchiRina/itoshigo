53a1b98964e86a78af4adbf77f3a2c948a1bc937
Rina Iwabuchi <anps.pyhu922@gmail.com>
2022-10-15 19:42:19 +0900

    カレンダー機能と誕生日と日記を紐づける

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,22 @@
 class CalendarController < ApplicationController
   def index
+    @pets = Pet.where(user_id: current_user.id)
+    pet_id = params[:pet_id]
+    posts = Post.where(pet_id: pet_id).where(status: 2)
+    @events = []
+    posts.each do |post|
+      @events.push({
+        title: post.title,
+        date: post.created_at.strftime("%Y-%m-%d"),
+        url: post_path(post.id),
+      })
+    end
+    if pet_id.present?
+      @events.push({
+        title: "#{pet.name}の誕生日",
+        date: pet.birthday.strftime("%Y-%m-%d"),
+        url:nil
+      })
+    end
   end
 end
