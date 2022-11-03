334c59fe0701b8b45696f7b2e1f02e67d39f3181
Rina Iwabuchi <anps.pyhu922@gmail.com>
2022-11-03 20:55:14 +0900

    各ページのレイアウト調整

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,15 @@
 class SearchController < ApplicationController
+  def index
+    @keyword = params[:q]
+    @posts = Post.where(status: 2)
+
+    if @keyword.present?
+      @posts = @posts
+        .where('title like ?', "%#{@keyword}%")
+        .or(
+          Post.where(status:2)
+        .where('content like ?', "%#{@keyword}%")
+        )
+    end
+  end
 end
